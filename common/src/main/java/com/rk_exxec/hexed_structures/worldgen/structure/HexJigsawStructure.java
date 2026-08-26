d1188a65acbc34b709b5687861ec6f07f3001c4f
rk-exxec <40037381+rk-exxec@users.noreply.github.com>
2026-08-26 22:56:54 +0200

    add height range field

--- common/src/main/java/com/rk_exxec/hexed_structures/worldgen/structure/HexJigsawStructure.java
+++ common/src/main/java/com/rk_exxec/hexed_structures/worldgen/structure/HexJigsawStructure.java
@@ -9,7 +9,6 @@ import com.mojang.serialization.codecs.RecordCodecBuilder;
 import com.rk_exxec.hexed_structures.Constants;
 import com.rk_exxec.hexed_structures.worldgen.HexWorldgenContext;
 import com.rk_exxec.hexed_structures.worldgen.structure.pools.HexJigsawPlacement;
-import com.rk_exxec.hexed_structures.worldgen.structure.ModStructureTypes;
 
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Holder;
@@ -20,7 +19,6 @@ import net.minecraft.world.level.levelgen.WorldGenerationContext;
 import net.minecraft.world.level.levelgen.heightproviders.HeightProvider;
 import net.minecraft.world.level.levelgen.structure.Structure;
 import net.minecraft.world.level.levelgen.structure.StructureType;
-import net.minecraft.world.level.levelgen.structure.pools.JigsawPlacement;
 import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
 
 /**
@@ -28,6 +26,27 @@ import net.minecraft.world.level.levelgen.structure.pools.StructureTemplatePool;
  * Copy of JigsawStructure with overwritten findGenerationPoint method for hexlands specific changes
  */
 public class HexJigsawStructure extends Structure {
+
+    public record Bounds(int lower, int upper) {
+        public static final int DEFAULT_LOWER = Integer.MIN_VALUE;
+        public static final int DEFAULT_UPPER = Integer.MAX_VALUE;
+
+        public Bounds() {
+            this(DEFAULT_LOWER, DEFAULT_UPPER);
+        }
+    }
+
+    public static final Codec<Bounds> BOUNDS_CODEC =
+        RecordCodecBuilder.create(instance ->
+            instance.group(
+                Codec.INT.optionalFieldOf("min", Bounds.DEFAULT_LOWER)
+                    .forGetter(Bounds::lower),
+
+                Codec.INT.optionalFieldOf("max", Bounds.DEFAULT_UPPER)
+                    .forGetter(Bounds::upper)
+            ).apply(instance, Bounds::new)
+        );
+        
     public static final Codec<HexJigsawStructure> CODEC = RecordCodecBuilder.create(instance ->
         instance.group(
             settingsCodec(instance),
@@ -37,7 +56,8 @@ public class HexJigsawStructure extends Structure {
             HeightProvider.CODEC.fieldOf("start_height").forGetter(structure -> structure.startHeight),
             Codec.BOOL.fieldOf("use_expansion_hack").forGetter(structure -> structure.useExpansionHack),
             Heightmap.Types.CODEC.optionalFieldOf("project_start_to_heightmap").forGetter(structure -> structure.projectStartToHeightmap),
-            Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter)
+            Codec.intRange(1, 128).fieldOf("max_distance_from_center").forGetter(structure -> structure.maxDistanceFromCenter),
+            BOUNDS_CODEC.optionalFieldOf("valid_height_range", new Bounds()).forGetter(structure -> structure.heightRange)
         ).apply(instance, HexJigsawStructure::new)
     );
 
@@ -48,8 +68,11 @@ public class HexJigsawStructure extends Structure {
     private final boolean useExpansionHack;
     private final Optional<Heightmap.Types> projectStartToHeightmap;
     private final int maxDistanceFromCenter;
+    private final Bounds heightRange;
 
-    public HexJigsawStructure(Structure.StructureSettings settings, Holder<StructureTemplatePool> startPool, Optional<ResourceLocation> startJigsawName,int maxDepth,HeightProvider startHeight,boolean useExpansionHack,Optional<Heightmap.Types> projectStartToHeightmap,int maxDistanceFromCenter)
+    public Bounds heightRange() {return this.heightRange;}
+
+    public HexJigsawStructure(Structure.StructureSettings settings, Holder<StructureTemplatePool> startPool, Optional<ResourceLocation> startJigsawName,int maxDepth,HeightProvider startHeight,boolean useExpansionHack,Optional<Heightmap.Types> projectStartToHeightmap,int maxDistanceFromCenter,Bounds heightRange)
     {
         super(settings);
         this.startPool = startPool;
@@ -59,12 +82,15 @@ public class HexJigsawStructure extends Structure {
         this.useExpansionHack = useExpansionHack;
         this.projectStartToHeightmap = projectStartToHeightmap;
         this.maxDistanceFromCenter = maxDistanceFromCenter;
+        this.heightRange = heightRange;
     }
 
     @Override
     public Optional<Structure.GenerationStub> findGenerationPoint(Structure.GenerationContext context) {
         ChunkPos chunkPos = context.chunkPos();
         int startY = this.startHeight.sample(context.random(), new WorldGenerationContext(context.chunkGenerator(), context.heightAccessor()));
+        // stop generation if not in height range
+        if(startY < heightRange.lower() || startY > heightRange.upper()) return Optional.empty();
         BlockPos startPos = new BlockPos(chunkPos.getMinBlockX(), startY, chunkPos.getMinBlockZ());
         // -- custom part start
         // pull hex settings if available, fallback to "vanilla" behavior otherwise
